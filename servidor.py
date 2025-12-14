43fd57c215220230bf22e1cfd36bace07586a961
Beel04 <milagrosmabelmedina@gmail.com>
2025-12-13 20:58:54 -0500

    Add files via upload

---
+++ servidor.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+
+HOST = "0.0.0.0"
+PORT = 5000
+
+clientes = []
+
+def manejar_cliente(conn):
+    while True:
+        try:
+            data = conn.recv(1024)
+            if not data:
+                break
+            for c in clientes:
+                if c != conn:
+                    c.sendall(data)
+        except:
+            break
+    conn.close()
+    clientes.remove(conn)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen(2)
+
+print("Servidor listo, esperando jugadores...")
+
+while True:
+    conn, addr = server.accept()
+    print("Jugador conectado:", addr)
+    clientes.append(conn)
+    threading.Thread(target=manejar_cliente, args=(conn,)).start()
